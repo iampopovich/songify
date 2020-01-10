cbc6142fd9ea96d7623f97366ee1a7646175aa1c
Alex <iampopovich@gail.com>
2020-01-10 09:41:56 +0300

    добавил в ндекс гитгно, чтоб его потом не востаавливать по памяти. добавил черновик метода обработки сообщений

--- bot.py
+++ bot.py
@@ -1,21 +1,29 @@
-#version: v0.0.1
-import telebot 
+#version: v0.0.2
+import telebot, telebot.Types 
 # import time
 import datetime
-
+import json
+import re
 
 def getConfig():
 	pass
 
-
 bot = telebot.TeleBot()
-
 
 def main():
 	pass
 
-
-
+@bot.message_handler(regex = "")
+def saveSong(message):
+	deadline = datetime.datetime.now() + datetime.timedelta(days = 7)
+	info = '{}\nПесня добавлена.\nТвой дедлайн : {}'.format(message.text, deadline)
+	kbStatus = Types.TeleBotCallbackKeoard()
+	textDoneButton = Types.TeleBotCallbackButton(text = 'Lyrics X', callback_data = 'Lyrics +')
+	tabsDoneButton = Types.TelebotCallbackButton(text = 'Tabs X', callback_data = 'Tabs +')
+	kbStatus.add(textDoneButton)
+	kbStatus.add(tabsDoneButton)
+	bot.send_message(message.chat.id, info, callback = kbStatus)
+	bot.delete_message(message.message_id)
 
 if __name__ == '__main__':
 	main()
