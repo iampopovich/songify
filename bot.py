17ddff5cefc7ffa0368f14760bbcad89b7834e68
Alex <iampopovich@gail.com>
2020-01-10 09:21:18 +0300

    прототип без сохраненя табов в локальный реп.

---
+++ bot.py
@@ -0,0 +1,21 @@
+#version: v0.0.1
+import telebot 
+# import time
+import datetime
+
+
+def getConfig():
+	pass
+
+
+bot = telebot.TeleBot()
+
+
+def main():
+	pass
+
+
+
+
+if __name__ == '__main__':
+	main()
